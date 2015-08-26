515fbd8f02a371c850bdbcfcefa4dd0f186b73a5
EJLarcs <ejlarcombe@gmail.com>
2015-08-26 15:53:19 -0400

    done

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require './environment'
-
+require 'pry'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+          erb :'root'
+    end
 
+    get '/new' do
+          erb :'pirates/new'
+    end
+
+    post '/pirates' do
+    
+      erb :'pirates/show'
+      # redirect '/pirates/new'
+    end
   end
 end
